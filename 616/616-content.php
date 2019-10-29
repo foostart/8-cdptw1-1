4a3acc9df8b4a6a4bfec789601d0b0d57a935536
dailuong99 <luongtandai2903@gmail.com>
2019-10-29 13:18:48 +0700

    v1

--- 616/616-content.php
+++ 616/616-content.php
@@ -57,7 +57,7 @@ $url_path = str_replace('\\', '/', $url_path);
                     <span class="required-lbl">* </span><label class="control-label" for="shippingcountry">Shipping Country</label>
                     <div class="controls">
                         <div class="controls">
-                            <select id="shippingcountry" name="shippingcountry" class="input-xlarge">
+                             <select id="inputState" class="form-control">
                                 <option>Please Select</option>
                                 <option>Australia</option>
                             </select>
@@ -68,7 +68,7 @@ $url_path = str_replace('\\', '/', $url_path);
                 <div class="form-group col-md-6 pd0">
                     <span class="required-lbl">* </span><label class="control-label" for="shippingstate">Shipping State</label>
                     <div class="controls">
-                        <select id="shippingstate" name="shippingstate" class="input-xlarge">
+                         <select id="inputState" class="form-control">
                             <option>Please Select</option>
                             <option>Australian Capital Territory</option>
                             <option>New South Wales</option>
@@ -143,188 +143,188 @@ $url_path = str_replace('\\', '/', $url_path);
                             <input id="cvv" name="cvv" type="text" placeholder="" class="form-control" required="">
                         </div>
                     </div>
-                    <div class="control-group col-md-4">
+                    <div class="control-group col-md-3 pd0">
                         <div class="control-group">
                             <label class="control-label" for="month">Expiration Date</label>
                             <div class="controls">
-                                <select id="month" name="month" class="input-xlarge">
-                                    <option>Select Month</option>
-                                    <option>01</option>
-                                    <option>02</option>
-                                    <option>03</option>
-                                    <option>04</option>
-                                    <option>05</option>
-                                    <option>06</option>
-                                    <option>07</option>
-                                    <option>08</option>
-                                    <option>09</option>
-                                    <option>10</option>
-                                    <option>11</option>
-                                    <option>12</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="control-group col-md-4">
-                        <div class="control-group">
-                            <label class="control-label" for="selectyear"></label>
-                            <div class="controls">
-                                <select id="selectyear" name="selectyear" class="input-xlarge">
-                                    <option>Select Year</option>
-                                    <option>14</option>
-                                    <option>15</option>
-                                    <option>16</option>
-                                    <option>17</option>
-                                    <option>18</option>
-                                    <option>19</option>
-                                    <option>20</option>
-                                    <option>21</option>
-                                    <option>22</option>
-                                    <option>23</option>
-                                    <option>24</option>
-                                    <option>25</option>
-                                    <option>26</option>
-                                    <option>27</option>
-                                    <option>28</option>
-                                    <option>29</option>
-                                    <option>30</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="form-group col-md-6">
-                </div>
-
-                <hr/>
-
-                <div class="form-group col-md-12 bg-primary">
-                    <label class="control-label" for="billinginformation">Billing Address</label>
-                </div>
-
-                <div class="form-group col-md-6 pd0">
-                    <span class="required-lbl">* </span><label class="control-label" for="billingaddress1">Address 1</label>
-                    <div class="controls">
-                        <input id="billingaddress1" name="billingaddress1" type="text" placeholder="" class="form-control" required="">
-                    </div>
-                </div>
-
-                <div class="form-group col-md-6 pd0">
-                    <label class="control-label" for="billingaddress2">Address 2</label>
-                    <div class="controls">
-                        <input id="billingaddress2" name="billingaddress2" type="text" placeholder="" class="form-control" required="">
-                    </div>
-                </div>
-
-                <div class="form-group col-md-6 pd0">
-                    <span class="required-lbl">* </span><label class="control-label" for="billingcountry">Billing Country</label>
-                    <div class="controls">
-                        <div class="controls">
-                            <select id="billingcountry" name="billingcountry" class="input-xlarge">
-                                <option>Please Select</option>
-                                <option>Australia</option>
+                              <select id="inputState" class="form-control">
+                                <option>Select Month</option>
+                                <option>01</option>
+                                <option>02</option>
+                                <option>03</option>
+                                <option>04</option>
+                                <option>05</option>
+                                <option>06</option>
+                                <option>07</option>
+                                <option>08</option>
+                                <option>09</option>
+                                <option>10</option>
+                                <option>11</option>
+                                <option>12</option>
                             </select>
                         </div>
                     </div>
                 </div>
 
-                <div class="form-group col-md-6 pd0">
-                    <span class="required-lbl">* </span><label class="control-label" for="billingstate">Billing State</label>
-                    <div class="controls">
-                        <select id="billingstate" name="billingstate" class="input-xlarge">
-                            <option>Please Select</option>
-                            <option>Australian Capital Territory</option>
-                            <option>New South Wales</option>
-                            <option>Northern Territory</option>
-                            <option>Queensland</option>
-                            <option>South Australia</option>
-                            <option>Tasmania</option>
-                            <option>Victoria</option>
-                            <option>Western Australia</option>
-                            <option>Other</option>
+                <div class="control-group col-md-3 pd0">
+                    <div class="control-group">
+                        <label class="control-label" for="selectyear">Select Year</label>
+                        <div class="controls">
+                          <select id="inputState" class="form-control">
+                            <option>Select Year</option>
+                            <option>14</option>
+                            <option>15</option>
+                            <option>16</option>
+                            <option>17</option>
+                            <option>18</option>
+                            <option>19</option>
+                            <option>20</option>
+                            <option>21</option>
+                            <option>22</option>
+                            <option>23</option>
+                            <option>24</option>
+                            <option>25</option>
+                            <option>26</option>
+                            <option>27</option>
+                            <option>28</option>
+                            <option>29</option>
+                            <option>30</option>
                         </select>
                     </div>
                 </div>
+            </div>
 
-                <div class="form-group col-md-6 pd0">
-                    <span class="required-lbl">* </span><label class="control-label" for="billingcity">Billing City</label>
-                    <div class="controls">
-                        <input id="billingcity" name="billingcity" type="text" placeholder="" class="form-control" required="">
-                    </div>
-                </div>
+        </div>
 
-                <div class="form-group col-md-6 pd0">
-                    <span class="required-lbl">* </span><label class="control-label" for="billingpostcode">Post Code</label>
-                    <div class="controls">
-                        <input id="billingpostcode" name="billingpostcode" type="text" placeholder="" class="form-control" required="">
-                    </div>
-                </div>
+        <div class="form-group col-md-6">
+        </div>
 
-                <div class="form-group col-md-12 bg-primary late">
-                    <label class="control-label" for="contactinformation">Contact Information:</label>
-                    <input id="contactinformation" style="display:none">
-                </div>
+        <hr/>
 
-                <div class="form-group col-md-6 pd0">
-                    <span class="required-lbl">* </span><label class="control-label" for="emailaddress">Email Address</label>
-                    <input id="emailaddress" style="display:none">
-                    <div class="controls">
-                        <input id="email" name="email" type="email" placeholder="" class="form-control" required="">
-                    </div>
-                </div>
+        <div class="form-group col-md-12 bg-primary">
+            <label class="control-label" for="billinginformation">Billing Address</label>
+        </div>
 
-                <div class="form-group col-md-6 pd0">
-                    <label class="control-label" for="phone">Phone</label>
-                    <div class="controls">
-                        <input id="phone" name="phone" type="number" placeholder="" class="form-control" required="">
-                    </div>
-                </div>
-
-                <div class="form-group col-md-6 pd0">
-                    <label class="control-label" for="organization">Organization</label>
-                    <div class="controls">
-                        <input id="organization" name="organization" type="text" placeholder="" class="form-control" required="">
-                    </div>
-                </div>
-
-                <div class="form-group col-md-12 bg-primary late">
-                    <label class="control-label" for="contactinformation">Additional Information:</label>
-                </div>
-
-                <label class="content-late">* To avoid duplication, Type "YES" if you are a current subscriber wishing to extend your subscription OR "NO" if you don’t have an active subscription</label>
-
-                <div class="form-group col-md-6 pd0">
-                    <div class="controls">
-                        <input id="additionalinfo" name="additionalinfo" type="text" placeholder="" class="form-control" required="">
-                    </div>
-                </div>
-
-                <div class="form-group col-md-6" style="padding-left: 20px;margin-top: -14px;">
-                    <div class="control-group">
-                        <label class="control-label" for="iaccept"></label>
-                        <input id="iaccept" style="display:none">
-                        <div class="controls">
-                            <label class="checkbox" for="iaccept-0">
-                                <input type="checkbox" name="iaccept" id="iaccept-0" value="I accept the Teams and conditions">
-                                I accept the <a href="#">Teams and conditions</a>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group col-md-12">
-                    <div class="control-group confirm-btn">
-                        <label class="control-label" for="placeorderbtn"></label>
-                        <div class="controls">
-                            <button id="placeorderbtn" name="placeorderbtn" class="btn btn-primary">Place My Order</button>
-                        </div>
-                    </div>
-                </div>
-
+        <div class="form-group col-md-6 pd0">
+            <span class="required-lbl">* </span><label class="control-label" for="billingaddress1">Address 1</label>
+            <div class="controls">
+                <input id="billingaddress1" name="billingaddress1" type="text" placeholder="" class="form-control" required="">
             </div>
         </div>
+
+        <div class="form-group col-md-6 pd0">
+            <label class="control-label" for="billingaddress2">Address 2</label>
+            <div class="controls">
+                <input id="billingaddress2" name="billingaddress2" type="text" placeholder="" class="form-control" required="">
+            </div>
+        </div>
+
+        <div class="form-group col-md-6 pd0">
+            <span class="required-lbl">* </span><label class="control-label" for="billingcountry">Billing Country</label>
+            <div class="controls">
+                <div class="controls">
+                    <select id="inputState" class="form-control">
+                        <option>Please Select</option>
+                        <option>Australia</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group col-md-6 pd0">
+            <span class="required-lbl">* </span><label class="control-label" for="billingstate">Billing State</label>
+            <div class="controls">
+                 <select id="inputState" class="form-control">
+                    <option>Please Select</option>
+                    <option>Australian Capital Territory</option>
+                    <option>New South Wales</option>
+                    <option>Northern Territory</option>
+                    <option>Queensland</option>
+                    <option>South Australia</option>
+                    <option>Tasmania</option>
+                    <option>Victoria</option>
+                    <option>Western Australia</option>
+                    <option>Other</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group col-md-6 pd0">
+            <span class="required-lbl">* </span><label class="control-label" for="billingcity">Billing City</label>
+            <div class="controls">
+                <input id="billingcity" name="billingcity" type="text" placeholder="" class="form-control" required="">
+            </div>
+        </div>
+
+        <div class="form-group col-md-6 pd0">
+            <span class="required-lbl">* </span><label class="control-label" for="billingpostcode">Post Code</label>
+            <div class="controls">
+                <input id="billingpostcode" name="billingpostcode" type="text" placeholder="" class="form-control" required="">
+            </div>
+        </div>
+
+        <div class="form-group col-md-12 bg-primary late">
+            <label class="control-label" for="contactinformation" style="margin-left:-8px">Contact Information:</label>
+            <input id="contactinformation" style="display:none">
+        </div>
+
+        <div class="form-group col-md-6 pd0">
+            <span class="required-lbl">* </span><label class="control-label" for="emailaddress">Email Address</label>
+            <input id="emailaddress" style="display:none">
+            <div class="controls">
+                <input id="email" name="email" type="email" placeholder="" class="form-control" required="">
+            </div>
+        </div>
+
+        <div class="form-group col-md-6 pd0">
+            <label class="control-label" for="phone">Phone</label>
+            <div class="controls">
+                <input id="phone" name="phone" type="number" placeholder="" class="form-control" required="">
+            </div>
+        </div>
+
+        <div class="form-group col-md-6 pd0">
+            <label class="control-label" for="organization">Organization</label>
+            <div class="controls">
+                <input id="organization" name="organization" type="text" placeholder="" class="form-control" required="">
+            </div>
+        </div>
+
+        <div class="form-group col-md-12 bg-primary late">
+            <label class="control-label" for="contactinformation" style="margin-left:-8px">Additional Information:</label>
+        </div>
+
+        <label class="content-late">* To avoid duplication, Type "YES" if you are a current subscriber wishing to extend your subscription OR "NO" if you don’t have an active subscription</label>
+
+        <div class="form-group col-md-6 pd0">
+            <div class="controls">
+                <input id="additionalinfo" name="additionalinfo" type="text" placeholder="" class="form-control" required="">
+            </div>
+        </div>
+
+        <div class="form-group col-md-6" style="padding-left: 20px;margin-top: -14px;">
+            <div class="control-group">
+                <label class="control-label" for="iaccept"></label>
+                <input id="iaccept" style="display:none">
+                <div class="controls">
+                    <label class="checkbox" for="iaccept-0">
+                        <input type="checkbox" name="iaccept" id="iaccept-0" value="I accept the Teams and conditions">
+                        I accept the <a href="#">Teams and conditions</a>
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group col-md-12">
+            <div class="control-group confirm-btn">
+                <label class="control-label" for="placeorderbtn"></label>
+                <div class="controls">
+                    <button id="placeorderbtn" name="placeorderbtn" class="btn btn-primary">Place My Order</button>
+                </div>
+            </div>
+        </div>
+
     </div>
+</div>
+</div>
 </div>
